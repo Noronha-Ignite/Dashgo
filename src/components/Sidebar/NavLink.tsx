f66e9976622734a8147bbe8bea91bb05e8b91838
NoronhaGCP <inc.691@gmail.com>
2021-11-09 13:41:33 -0300

    Add routing to buttons

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,18 +1,22 @@
-import { Text, Link, Icon, LinkProps as ChakraLinkProps } from '@chakra-ui/react';
+import { Text, Link as ChakraLink, Icon, LinkProps as ChakraLinkProps } from '@chakra-ui/react';
 import { IconType } from 'react-icons';
+import { ActiveLink } from '../ActiveLink';
 
 interface NavLinkProps extends ChakraLinkProps {
   children: string;
   icon: IconType;
+  href: string;
 }
 
-export function NavLink({ children, icon, ...rest }: NavLinkProps) {
+export function NavLink({ children, icon, href, ...rest }: NavLinkProps) {
   return (
-    <Link display='flex' align='center' {...rest}>
-      <Icon as={icon} fontSize='20'/>
-      <Text ml='4' fontWeight='medium'>
-        { children }
-      </Text>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display='flex' align='center' {...rest}>
+        <Icon as={icon} fontSize='20'/>
+        <Text ml='4' fontWeight='medium'>
+          { children }
+        </Text>
+      </ChakraLink>
+    </ActiveLink>
   );
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import {
   Flex,
   Box,
@@ -12,14 +13,21 @@ import {
   Th,
   Td,
   Checkbox,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+
 
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -33,32 +41,34 @@ export default function UserList() {
               Usuários
             </Heading>
 
-            <Button
-              as='a'
-              size='sm'
-              fontSize='sm'
-              colorScheme='pink'
-              leftIcon={<Icon as={RiAddLine} fontSize='20'/>}
-              cursor='pointer'
-            >
-              Criar novo
-            </Button>
+            <Link href='/users/create' passHref>
+              <Button
+                as='a'
+                size='sm'
+                fontSize='sm'
+                colorScheme='pink'
+                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+                cursor='pointer'
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme='whiteAlpha'>
             <Thead>
               <Tr>
-                <Th px='6' color='gray.300' w='8'>
+                <Th px={['4', '4', '6']} color='gray.300' w='8'>
                   <Checkbox colorScheme='pink' />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
                 <Th w='6'></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px='6'>
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme='pink' />
                 </Td>
                 <Td>
@@ -69,22 +79,22 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
                 <Td>
-                <Button
-                  as='a'
-                  size='sm'
-                  fontSize='sm'
-                  colorScheme='purple'
-                  leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
-                  cursor='pointer'
-                >
-                  Editar
-                </Button>
+                  <Button
+                    as='a'
+                    size='sm'
+                    fontSize='sm'
+                    colorScheme='purple'
+                    leftIcon={isWideVersion && <Icon as={RiPencilLine} fontSize='16' mx={!isWideVersion && '0'} />}
+                    cursor='pointer'
+                  >
+                    {isWideVersion ? 'Editar' : <Icon as={RiPencilLine} fontSize='16' mx={!isWideVersion && '0'} />}
+                  </Button>
                 </Td>
               </Tr>
               <Tr>
-                <Td px='6'>
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme='pink' />
                 </Td>
                 <Td>
@@ -95,18 +105,19 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td>06 de Abril, 2021</Td>
+                {isWideVersion && <Td>06 de Abril, 2021</Td>}
                 <Td>
-                <Button
-                  as='a'
-                  size='sm'
-                  fontSize='sm'
-                  colorScheme='purple'
-                  leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
-                  cursor='pointer'
-                >
-                  Editar
-                </Button>
+                  <Button
+                    as='a'
+                    size='sm'
+                    fontSize='sm'
+                    colorScheme='purple'
+                    leftIcon={isWideVersion && <Icon as={RiPencilLine} fontSize='16' mx={!isWideVersion && '0'} />}
+                    cursor='pointer'
+                    
+                  >
+                    {isWideVersion ? 'Editar' : <Icon as={RiPencilLine} fontSize='16' mx={!isWideVersion && '0'} />}
+                  </Button>
                 </Td>
               </Tr>
             </Tbody>
