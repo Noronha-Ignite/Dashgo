28bc1723200dacc00e22fae8960eeadfebd86710
NoronhaGCP <inc.691@gmail.com>
2021-12-02 10:34:13 -0300

    configure miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { SidebarDrawerContextWrapper } from '../hooks/useSidebarDrawer';
 
+import { makeServer } from '../services/mirage';
+
 import { theme } from '../styles/theme';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
